bbf23df9f9c64a494a2879f2f5ab6ebcd55ca082
milas <supeeva@yandex.ru>
2022-11-18 04:37:19 +0400

    Задача с боксёрами

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        var friend = 19;
-        friend = friend + 2;
-        System.out.println(friend);
-        friend = friend / 7;
-        System.out.println(friend);
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+       var firstBoxer = 78.2;
+       var secondBoxer = 82.7;
+       var totalWeight = firstBoxer + secondBoxer;
+       System.out.println("Общий вес боксёров " + totalWeight + "кг");
+
+       var difference = secondBoxer - firstBoxer;
+       System.out.println("Разница в весе боксёров " + difference + "кг");
+
+       var remainderDifference = secondBoxer % firstBoxer;
+       System.out.println("Разница в весе боксёров (остаток от деления) " + remainderDifference + "кг");
         
 
     }
